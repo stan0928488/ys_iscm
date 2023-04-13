c1b47d33dd749aca196a5bad801b42ee271ec16f
WALSIN\ur10369 <chienwen_chang@walsin.com>
2023-04-13 13:43:20 +0800

    1.調整機台負荷直棒/非直棒

--- src/app/services/config/config.service.ts
+++ src/app/services/config/config.service.ts
@@ -32,36 +32,13 @@ export class ConfigService {
 
     switch (hostName) {
       case "localhost":
-         if(flag === "1") { 
+        //  if(flag === "1") { 
           this.APIURL = `http://${hostName}:8080${APIBODY}`;
-         } else {
-           var urlHost = "ys-webapt1.walsin.com"
-           this.APIURL = `http://${urlHost}:8080${APIBODY}`;
-         }
-       
+        //  } else {
+        //    var urlHost = "ys-webapt1.walsin.com"
+        //    this.APIURL = `http://${urlHost}:8080${APIBODY}`;
+        //  }
       break;
-      case "10.106.200.96":
-        if(flag === "1") { 
-          //var urlHost = "ys-webapt1.walsin.com"
-          this.APIURL = `http://${hostName}:8080${APIBODY}`;
-        } else {
-          var urlHost = "ys-webapt1.walsin.com"
-          this.APIURL = `http://${urlHost}:8080${APIBODY}`;
-        }
-        
-        break;
-      case "apptst.walsin.com":
-        this.APIURL = `http://${hostName}:8083${APIBODY}`;
-        break;
-      case "ys-webapt1.walsin.com":
-        this.APIURL = `http://${hostName}:8080${APIBODY}`;
-        break;
-      case "ys-pps.walsin.corp":
-        this.APIURL = `http://${hostName}:8080${APIBODY}`;
-        break;
-      case "10.106.2.153":
-        this.APIURL = `http://${hostName}:80${APIBODY}`;
-        break;
       case "ys-webapp.walsin.com":
         this.APIURL = `https://${hostName}${APIBODY}`;
         break;
