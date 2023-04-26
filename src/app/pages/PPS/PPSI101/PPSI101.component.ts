782bb20d2b78c959204b9bf9cb840e270e14da07
Alex_Kao <alex_kao@wistronits.com>
2023-04-26 11:33:00 +0800

    按鈕增加 匯入匯出

--- src/app/pages/PPS/PPSI101/PPSI101.component.ts
+++ src/app/pages/PPS/PPSI101/PPSI101.component.ts
@@ -6,8 +6,8 @@ import {NzMessageService} from "ng-zorro-antd/message"
 import {NzModalService} from "ng-zorro-antd/modal"
 import * as moment from 'moment';
 import * as _ from "lodash";
-
-
+import * as XLSX from 'xlsx';
+import { ExcelService } from "src/app/services/common/excel.service";
 interface ItemData1 {
   id: string;
   tab1ID: number;
@@ -45,6 +45,12 @@ export class PPSI101Component implements AfterViewInit {
   PPSINP01List: ItemData1[] = [];
   displayPPSINP01List: ItemData1[] = [];
   
+  file:File;
+  inputFileUseInUpload;
+  arrayBuffer:any;
+  importdata = [];
+  titleArray = ["鋼種","特殊機台使用","鋼種類別"];
+  importdata_repeat = [];
 
   constructor(
     private PPSService: PPSService,
@@ -52,6 +58,7 @@ export class PPSI101Component implements AfterViewInit {
     private cookieService: CookieService,
     private message: NzMessageService,
     private Modal: NzModalService,
+    private excelService: ExcelService,
   ) {
     this.i18n.setLocale(zh_TW);
     this.USERNAME = this.cookieService.getCookie("USERNAME");
@@ -375,5 +382,150 @@ export class PPSI101Component implements AfterViewInit {
   }
   
 
+    // excel檔名
+    incomingfile(event) {
+      this.file = event.target.files[0]; 
+      console.log("incomingfile e : " + this.file);
+      let lastname = this.file.name.split('.').pop();
+      if (lastname !== 'xlsx' && lastname !== 'xls' && lastname !== 'csv') {
+        this.errorMSG('檔案格式錯誤', '僅限定上傳 Excel 格式。');
+        this.clearFile();
+        return;
+      }
+    }
+
+    clearFile() {
+      document.getElementsByTagName('input')[0].value = '';
   
+    }
+
+    Upload() {
+    
+      let getFileNull = this.inputFileUseInUpload;
+      if(getFileNull === undefined){
+        this.errorMSG('請選擇檔案', '');
+        return;
+      }
+  
+      let lastname = this.file.name.split('.').pop();
+      console.log("this.file.name: "+this.file.name);
+      console.log("incomingfile e : " + this.file);
+      if (lastname !== 'xlsx' && lastname !== 'xls' && lastname !== 'csv') {
+        this.errorMSG('檔案格式錯誤', '僅限定上傳 Excel 格式。');
+        this.clearFile();
+        return;
+      } else {
+        console.log("上傳檔案格式沒有錯誤");
+        let fileReader = new FileReader();
+        fileReader.onload = (e) => {
+          this.arrayBuffer = fileReader.result;
+          var data = new Uint8Array(this.arrayBuffer);
+          var arr = new Array();
+          for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+          var bstr = arr.join("");
+          var workbook = XLSX.read(bstr, {type:"binary"});
+          var first_sheet_name = workbook.SheetNames[0];
+          var worksheet:any = workbook.Sheets[first_sheet_name];
+          this.importdata = XLSX.utils.sheet_to_json(worksheet, {raw:true});
+    
+          
+            console.log("importExcel")
+            console.log(this.importdata)
+            this.importExcel(this.importdata);
+          
+        }
+        fileReader.readAsArrayBuffer(this.file);
+      }
+    }
+
+    importExcel(_data) {
+
+      console.log("EXCEL 資料上傳檢核開始");
+      var upload_data = [];
+      for(let i=0 ; i < _data.length ; i++) {
+        console.log(_data[i]);
+
+        let allData = JSON.stringify(_data[i]);
+
+        if (this.importdata_repeat.includes(allData)){
+          this.errorMSG('重複資料', '第' + (i+2) + "筆與上一筆為重複資料");
+          this.clearFile();
+          return;
+        }
+        else{
+          this.importdata_repeat.push(allData);
+          console.log(_data[i]['特殊機台使用']);
+          if(_data[i]['特殊機台使用'] == undefined)
+            _data[i]['特殊機台使用'] = '';
+
+          console.log(_data[i]['特殊機台使用']);
+          upload_data.push({
+            GRADE_NO: _data[i]['鋼種'],
+            SPECIAL_EQUIP_CODE: _data[i]['特殊機台使用'] ,
+            GRADE_GROUP: _data[i]['鋼種類別'],
+            DATETIME : moment().format('YYYY-MM-DD HH:mm:ss'),
+            USERNAME : this.USERNAME,
+            PLANT_CODE : this.PLANT_CODE
+          })
+        }
+      }
+      
+
+      return new Promise((resolve, reject) => {
+        console.log("匯入開始");
+        this.LoadingPage = true;
+        let myObj = this;
+        let obj = {};
+        obj = {
+          EXCELDATA: upload_data
+        };
+
+        console.log("EXCELDATA:"+ obj);
+        myObj.PPSService.importI101TablExcel(obj).subscribe(res => {
+          console.log("importExcelPPSI101");
+          if(res[0].MSG === "Y") { 
+            
+
+            this.loading = false;
+            this.LoadingPage = false;
+            
+            this.sucessMSG("EXCCEL上傳成功", "");
+            this.clearFile();
+            this.getPPSINP01List()
+            
+          } else {
+            this.errorMSG("匯入錯誤", res[0].MSG);
+            this.clearFile();
+            this.loading = false;
+            this.LoadingPage = false;
+          }
+        },err => {
+          reject('upload fail');
+          this.errorMSG("修改存檔失敗", "後台存檔錯誤，請聯繫系統工程師");
+          this.loading = false;
+          this.LoadingPage = false;
+        })
+      });
+      this.getPPSINP01List();
+
+    }
+
+    convertToExcel() {
+      console.log("convertToExcel");
+      let arr = [];
+      console.log(this.displayPPSINP01List);
+      let fileName = `鋼種分類`;
+      
+      for(let i = 0 ; i<this.displayPPSINP01List.length ; i++ ){
+
+        var ppsInP01 = {
+          GRADE_NO: this.displayPPSINP01List[i].GRADE_NO,
+          GRADE_GROUP: this.displayPPSINP01List[i].GRADE_GROUP,
+          SPECIAL_EQUIP_CODE: this.displayPPSINP01List[i].SPECIAL_EQUIP_CODE
+        }
+        arr.push(ppsInP01);
+      }
+      
+      this.excelService.exportAsExcelFile(arr, fileName, this.titleArray);
+    }
 }
