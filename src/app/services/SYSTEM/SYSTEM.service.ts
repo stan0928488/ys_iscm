6f0558d3737e25482a5c83e841cb292c2ba275b0
WALSIN\ur11550 <yanmin_jian@walsin.com>
2023-12-21 12:21:26 +0800

    跨域

--- src/app/services/SYSTEM/SYSTEM.service.ts
+++ src/app/services/SYSTEM/SYSTEM.service.ts
@@ -15,6 +15,7 @@ export class SYSTEMService {
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
       //'accept-user': this.cookieService.getCookie('USERNAME'),
       //'plant-code': this.cookieService.getCookie('plantCode')
     }),
