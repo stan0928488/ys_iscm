464fc9887565ba56893cecd46ca95001d5d0e11a
WALSIN\ur10369 <chienwen_chang@walsin.com>
2024-01-29 16:00:43 +0800

    CSS 調整

--- src/app/main/recursion-menus.component.ts
+++ src/app/main/recursion-menus.component.ts
@@ -3,30 +3,35 @@ import { Component, INJECTOR, Input } from "@angular/core";
 @Component({
     selector: 'menus',
     template: `
-            <ng-container *ngFor="let menu of menus">
-                <li
-                    *ngIf="menu.isShow == 1 && menu.children && menu.children.length > 0"
-                    nz-submenu
-                    [nzOpen]="menu.open"
-                    [nzTitle]="menu.menuName"
-                    [nzIcon]="menu.icon"
-                    [nzDisabled]="menu.disabled"
-                    class="menuItemText">
-                    <ul>
-                        <menus 
-                            [menus]="menu.children">
-                        </menus>
-                    </ul>
-                </li>
-                <li
-                    *ngIf="menu.isShow == 1 && (!menu.children || menu.children.length <= 0)"
-                    nz-menu-item
-                    [nzDisabled]="menu.disabled"
-                    [nzSelected]="menu.selected">
-                    <span nz-icon [nzType]="menu.icon" *ngIf="menu.icon"></span>
-                    <a [routerLink]="[menu.path]"> {{menu.menuName}} </a>
-                </li>
-            </ng-container>
+    <ng-container *ngTemplateOutlet="menuTpl; context: { $implicit: menus }"></ng-container>
+    <ng-template #menuTpl let-menus>
+      <ng-container *ngFor="let menu of menus">
+            <li
+                *ngIf="!menu.children || menu.children.length <= 0"
+                nz-menu-item
+                [nzDisabled]="menu.disabled"
+                [nzSelected]="menu.selected"
+                [nzPaddingLeft]="menu.level * 24"
+            >
+            <span nz-icon [nzType]="menu.icon" *ngIf="menu.icon"></span>
+            <a [routerLink]="[menu.path]"> {{menu.menuName}} </a>
+            </li>
+            <li
+                *ngIf="menu.children && menu.children.length > 0"
+                nz-submenu
+                [nzOpen]="menu.open"
+                [nzTitle]="menu.menuName"
+                [nzIcon]="menu.icon"
+                [nzDisabled]="menu.disabled"
+                [nzPaddingLeft]="menu.level * 24"
+            >
+          <ul>
+            <ng-container *ngTemplateOutlet="menuTpl; context: { $implicit: menu.children }"></ng-container>
+          </ul>
+        </li>
+      </ng-container>
+    </ng-template>
+
     `,
   })
   export class RecursionMenusComponent {
