cabdc956a8f263306bdc34d9ee640a9db0b603ee
WALSIN\ur10369 <chienwen_chang@walsin.com>
2024-02-20 10:07:59 +0800

    fix. 增加表格讀取 / console.log 拿掉 / 表面料號 margin調整 / apiurl改用context_path / main icon 間距調整

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -124,9 +124,11 @@ import { UserProfileComponent } from './pages/USER/user-profile/user-profile.com
 
 import { PPSI205A401Component } from './pages/PPS/ppsi205-a401/ppsi205-a401.component';
 import { PPSI205A100Component } from './pages/PPS/ppsi205-a100/ppsi205-a100.component';
-import { ORPI001Component } from './pages/ORP/ORPI001/ORPI001.component';
 import { MainComponent } from './main/main.component';
 import { AgHeadSeetingComponent } from './pages/SYSTEM/ag-head-seeting/ag-head-seeting.component';
+
+import { ORPI001Component } from './pages/ORP/ORPI001/ORPI001.component';
+import { ORPI999Component } from './pages/ORP/ORPI999/ORPI999.component';
 
 
 const routes: Routes = [
@@ -466,10 +468,26 @@ const routes: Routes = [
             data : {
               pageName:"料號客戶需求碼對照表"
             }
+          },
+          { 
+            path: 'I999', 
+            component: ORPI999Component,
+            data : {
+              pageName:"表格讀取"
+            }
           }
         ],
         canActivate: [AuthGuard]
       },
+      {
+        path: 'ORPExe',
+        children: [
+          { path: '', redirectTo: '/main/ORPExe/P001', pathMatch: 'full' },
+          
+        ],
+        canActivate: [AuthGuard]
+      },
+
       /* RMP & PAS & DCM */
 
       /* DCM */
